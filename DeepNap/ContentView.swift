0a209086a0ae93523342d5cb4a4f9e1d5ea8f9d9
developerkamol <developerkamol@gmail.com>
2025-03-26 17:46:11 +0500

    UX more improved

--- DeepNap/ContentView.swift
+++ DeepNap/ContentView.swift
@@ -18,6 +18,10 @@ struct ContentView: View {
     @State private var alertMessage = ""
     @State private var showAlert = false
     
+    var recommendedBedtime: String {
+        calculateBedTime()
+    }
+    
     static var defaultWakeTime: Date {
         var components = DateComponents()
         components.hour = 7
@@ -28,59 +32,55 @@ struct ContentView: View {
     var body: some View {
         NavigationStack{
             Form {
-                VStack(alignment: .leading, spacing: 0) {
+                Section {
                     Text("When do you want to wake up?")
                         .font(.headline)
                     DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
                         .labelsHidden()
                 }
                 
-                VStack(alignment: .leading, spacing: 0){
+                Section {
                     Text("Desired amount of sleep")
                         .font(.headline)
                     Stepper("\(sleepAmount.formatted())", value: $sleepAmount, in: 4...12, step: 0.25)
                 }
                 
-                VStack(alignment: .leading, spacing: 0){
-                    Text("Daily coffee intake")
+                Section {
+                    Picker(selection: $coffeeAmount) {
+                        ForEach(0...10, id: \.self) {
+                            Text("^[\($0) cup](inflect: true)")
+                        }
+                    } label: {
+                        Text("Daily coffee intake")
+                            .font(.headline)
+                    }
+                }
+                
+                Section {
+                    Text("Your ideal bedtime is: \(recommendedBedtime)")
                         .font(.headline)
-                    Stepper("^[\(coffeeAmount) cup](inflect: true)", value: $coffeeAmount, in: 0...10)
                 }
             }
             .navigationTitle("DeepNap")
-            .toolbar {
-                Button("Calculate", action: calculateBedTime)
-            }
-            .alert(alertTitle, isPresented: $showAlert){
-                Button("OK"){}
-            } message: {
-                Text(alertMessage)
-            }
         }
     }
-    func calculateBedTime(){
-        do {
-            let config = MLModelConfiguration()
-            let model = try SleepCalculator(configuration: config)
-            
-            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
-            let hour = (components.hour ?? 0) * 60 * 60
-            let minutes = (components.minute ?? 0) * 60
-            
-            let prediction = try model.prediction(wake: Double(hour + minutes), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
-            
-            let sleepTime = wakeUp - prediction.actualSleep
-            
-            alertTitle = "Your ideal bedtime is..."
-            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
-        } catch {
-            alertTitle = "Error"
-            alertMessage = "Sorry there was a problem calculating your sleep time."
-            
+    func calculateBedTime() -> String {
+            do {
+                let config = MLModelConfiguration()
+                let model = try SleepCalculator(configuration: config)
+                
+                let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+                let hour = (components.hour ?? 0) * 60 * 60
+                let minutes = (components.minute ?? 0) * 60
+                
+                let prediction = try model.prediction(wake: Double(hour + minutes), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+                
+                let sleepTime = wakeUp - prediction.actualSleep
+                return sleepTime.formatted(date: .omitted, time: .shortened)
+            } catch {
+                return "Error calculating bedtime"
+            }
         }
-        showAlert = true
-    }
-    
 
 }
 
