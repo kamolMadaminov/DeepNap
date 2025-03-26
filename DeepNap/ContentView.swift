c66e725c85efc1948297386648dba778d4ec648e
developerkamol <developerkamol@gmail.com>
2025-03-26 09:19:31 +0500

    Initial Commit

---
+++ DeepNap/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DeepNap
+//
+//  Created by Kamol Madaminov on 26/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DeepNap/DeepNapApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeepNapApp.swift
+//  DeepNap
+//
+//  Created by Kamol Madaminov on 26/03/25.
+//
+
+import SwiftUI
+
+@main
+struct DeepNapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
